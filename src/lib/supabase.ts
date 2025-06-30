e8f0d9f332adf246efc4dde51b437cef6dd85ecf
qingmao-cell <youchiliao@gmail.com>
2025-06-30 16:45:50 +0900

    🐭检查bug

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,3 +1,6 @@
+console.log("URL =", import.meta.env.VITE_SUPABASE_URL);
+console.log("KEY =", import.meta.env.VITE_SUPABASE_ANON_KEY);
+
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
